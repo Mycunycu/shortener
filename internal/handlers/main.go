06d9bb4fad9b9ef6bb212c768736b7efd377ac7e
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2021-11-22 22:11:49 +0300

    fix statictest

--- internal/handlers/main.go
+++ internal/handlers/main.go
@@ -10,31 +10,30 @@ import (
 )
 
 var urls = make(map[string]string)
-var baseShortUrl = "http://localhost:8080/"
+var baseShortURL= "http://localhost:8080/"
 var id int64 = 0
 
 func ShortenURL(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		bOrigUrl, err := io.ReadAll(r.Body)
+		bOrigURL, err := io.ReadAll(r.Body)
 		defer r.Body.Close()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		if len(bOrigUrl) == 0 {
+		if len(bOrigURL) == 0 {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		sOrigUrl := string(bOrigUrl)
 		id := atomic.AddInt64(&id, 1)
 		idString := strconv.Itoa(int(id))
-		urls[idString] = sOrigUrl
+		urls[idString] = string(bOrigURL)
 
-		resp := baseShortUrl + idString
-		
+		resp := baseShortURL + idString
+
 		w.Header().Set("content-type", "text/html; charset=UTF-8")
 		w.WriteHeader(http.StatusCreated)
 		w.Write([]byte(resp))
