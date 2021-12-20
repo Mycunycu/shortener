4dbac7ba3d3876a9b60f4f9657a4f711ead588b6
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2021-12-21 00:36:46 +0300

    check

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,17 +4,16 @@ import (
 	"log"
 	"sync"
 
-	"github.com/Mycunycu/shortener/internal/helpers"
 	"github.com/caarlos0/env"
 )
 
 const (
-	DefaultServerAddress = "localhost:8080"
+	DefaultServerAddress = ":8080"
 	DefaultBaseURL       = "http://localhost:8080/"
 )
 
 type Config struct {
-	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
 }
 
@@ -34,9 +33,9 @@ func initialize() {
 			log.Fatalf("initialize config error: %v", err)
 		}
 
-		if !helpers.IsValidAddress(cfg.ServerAddress) || !helpers.IsValidAddress(cfg.BaseURL) {
-			cfg.ServerAddress = DefaultServerAddress
-			cfg.BaseURL = DefaultBaseURL
-		}
+		// if !helpers.IsValidAddress(cfg.ServerAddress) || !helpers.IsValidAddress(cfg.BaseURL) {
+		// 	cfg.ServerAddress = DefaultServerAddress
+		// 	cfg.BaseURL = DefaultBaseURL
+		// }
 	})
 }
