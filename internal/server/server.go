7d82689d94ff1fdaa56a8490e0e987980f4b0468
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2021-12-20 22:41:28 +0300

    check

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/asaskevich/govalidator"
 )
 
 type Server struct {
@@ -9,11 +11,18 @@ type Server struct {
 }
 
 func NewServer(port string, handler http.Handler) *Server {
+	addr := "localhost:8080"
+	if govalidator.IsPort(port) {
+		addr = port
+	}
+	
 	return &Server{
-		&http.Server{Addr: port, Handler: handler},
+		&http.Server{Addr: addr, Handler: handler},
 	}
 }
 
 func (s *Server) Run() error {
 	return s.ListenAndServe()
 }
+
+
