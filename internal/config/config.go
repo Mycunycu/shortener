b6a733d610a3e94350a6ba36dc8d58dd8e79dd79
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2022-02-11 17:37:14 +0300

    try

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@ type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"internal/repository/storage.txt"`
-	DatabaseDSN     string `env:"DATABASE_DSN" envDefault:"postgres://user:123@localhost:5432/practicum?sslmode=disable"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
 	CtxTimeout      int64  `env:"CTX_TIMEOUT" envDefault:"5"`
 	MigrationPath   string `env:"MIGRATION_PATH" envDefault:"file://internal/repository/migrations"`
 }
